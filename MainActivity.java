dd67f27bfd1459cad2c36350c341712827210edf
Mora95 <m_nl_10@hotmail.com>
2017-10-19 14:16:30 -0500

    Metodo onstar

--- MainActivity.java
+++ MainActivity.java
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
     public MainActivity() {
     }
 
